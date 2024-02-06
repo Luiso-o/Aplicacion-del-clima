2cdfa4bb4b41f531319ff0c513ebd98062c62e94
Luiso-o <cheportillo@gmail.com>
2024-02-06 21:02:40 +0100

    mejoras

--- main.js
+++ main.js
@@ -1,56 +1,75 @@
 const API_KEY = '77bbeb5891aeaad2a5e59dcb00d8dd2e';
 
-const fetchData = position => {
+const fetchData = async position => {
     const { latitude, longitude } = position.coords;
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${API_KEY}`);
+        const data = await response.json();
+        setWeatherData(data);
+    } catch (error) {
+        showError('Hubo un error al obtener los datos meteorológicos:', error);
+    }
+};
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
-     .then(response => response.json()) 
-     .then(data => {
-         setWeatherData(data);
-    
-        console.log(data)
-     })
-     .catch(error => {
-         showError('Hubo un error al obtener los datos meteorológicos:', error);
-     });
+const getCurrentTimeEmoji = () => {
+    const hours = new Date().getHours();
+    const greeting = document.getElementById('greeting');
+
+    if (hours >= 6 && hours < 12) {
+        greeting.textContent = '🌅 Good Morning! ☕'; 
+    } else if (hours >= 12 && hours < 18) {
+        greeting.textContent =  '🌞 Good Afternoon! 😎'; 
+    } else if (hours >= 18 && hours < 22) {
+        greeting.textContent =  '🌆 Good Evening! 😊'; 
+    } else {
+        greeting.textContent = '🌙 Good Night! 😴'; 
+    }
 }
 
-const setWeatherData = data => {
+document.addEventListener('DOMContentLoaded', () => {
+    getCurrentTimeEmoji();
+    setInterval(getCurrentTimeEmoji, 60000);
+});
+
+const getWeatherDescriptionWithEmoji = (description) => {
+    const emojiMap = {
+        'Clear': '☀️ Sunny',
+        'Clouds': '☁️ Cloudy',
+        'Rain': '🌧️ Rainy',
+        'Drizzle': '🌦️ Drizzly',
+        'Thunderstorm': '⛈️ Thunderstorm',
+        'Snow': '❄️ Snowy',
+    };
+    return emojiMap[description] || description;
+};
+
+const setWeatherData = (data) => {
     const weatherData = {
-        location: '📍 ' + data.name,
+        location: `📍 ${data.name}`,
         description: data.weather[0].main,
-        temperature: '🌡️ ' + data.main.temp + ' °C',
-        feelsLike: '🌡️ Sensación térmica: ' + data.main.feels_like + ' °C',
-        humidity: '💧 Humedad: ' + data.main.humidity + '%',
-        pressure: '🔵 Presión: ' + data.main.pressure + ' hPa',
-        tempMax: '🔺 Temp. Máxima: ' + data.main.temp_max + ' °C',
-        tempMin: '🔻 Temp. Mínima: ' + data.main.temp_min + ' °C',
-        windSpeed: '💨 Velocidad del viento: ' + data.wind.speed + ' m/s',
+        temperature: `🌡️ ${data.main.temp} °C`,
+        feelsLike: `🥵🥶 Sensación térmica: ${data.main.feels_like} °C`,
+        humidity: `💧 Humedad: ${data.main.humidity}%`,
+        pressure: `🔵 Presión: ${data.main.pressure} hPa`,
+        tempMax: `🔺 Temp. Máxima: ${data.main.temp_max} °C`,
+        tempMin: `🔻 Temp. Mínima: ${data.main.temp_min} °C`,
+        windSpeed: `🌬️ Velocidad del viento: ${data.wind.speed} m/s`,
         date: getDate(),
     };
 
-    Object.keys(weatherData).forEach(key => {
-       const element = document.getElementById(key);
-       if (element) {
-           if (key === 'description') {
-               const emojiMap = {
-                'Clear': '☀️ Sunny',
-                'Clouds': '☁️ Cloudy',
-                'Rain': '🌧️ Rainy',
-                'Drizzle': '🌦️ Drizzly',
-                'Thunderstorm': '⛈️ Thunderstorm',
-                'Snow': '❄️ Snowy',
-               };
-               const descriptionEmoji = emojiMap[weatherData[key]] || '';
-               element.textContent = descriptionEmoji;
-           } else {
-               element.textContent = weatherData[key];
-           }
-       }
+    Object.keys(weatherData).forEach((key) => {
+        const element = document.getElementById(key);
+        if (element) {
+            if (key === 'description') {
+                element.textContent = getWeatherDescriptionWithEmoji(weatherData[key]);
+            } else {
+                element.textContent = weatherData[key];
+            }
+        }
     });
-
+    updateWeatherGif(data.main.temp);
     cleanUp();
-}
+};
 
 const cleanUp = () => {
     let container = document.getElementById('container');
@@ -82,7 +101,6 @@ const getDate = () => {
     };
 
     const monthEmoji = monthEmojis[month] || '';
-
     return `${monthEmoji} ${day}-${month}-${year}`;
 }
 
@@ -119,4 +137,14 @@ function getSeason() {
     }
 }
 
-
+function updateWeatherGif(temperature) {
+    const gif = document.getElementById('gif');
+    
+    if (temperature < 10) {
+        gif.src = 'images/frio.gif';
+    } else if (temperature >= 10 && temperature <= 25) {
+        gif.src = 'images/buenDia.gif';
+    } else {
+        gif.src = 'images/calor.gif';
+    }
+}
